07963734922ee124df3f7bb02188759a1d99a308
bakivh <dnizhade@gmail.com>
2022-02-22 23:08:11 +0300

    дз8 моки товаров и функция фильтрации

---
+++ mocks/list.js
@@ -0,0 +1,44 @@
+const movies = [
+  {
+    id: 1,
+    title: "Челюсти 1",
+    type: "ужасы",
+    director: "Спилберг",
+    status: "watched",
+  },
+  {
+    id: 1,
+    title: "Челюсти 2",
+    type: "ужасы",
+    director: "Спилберг",
+    status: "downloaded",
+  },
+  {
+    id: 1,
+    title: "Челюсти 3",
+    type: "ужасы",
+    director: "Спилберг",
+    status: "marked to watch",
+  },
+  {
+    id: 1,
+    title: "Челюсти 4",
+    type: "ужасы",
+    director: "Спилберг",
+    status: "marked not to watch",
+  },
+  {
+    id: 1,
+    title: "Челюсти 5",
+    type: "ужасы",
+    director: "Спилберг",
+    status: "watched",
+  },
+  {
+    id: 1,
+    title: "Титаник",
+    type: "драма",
+    director: "Кэмерон",
+    status: "watched",
+  },
+];
